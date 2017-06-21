a67c6c7b4f1a991a2e9877b9f614116168bd28b1
João Tavares Filho Segundo <joao@joao.com>
2017-06-21 20:58:41 -0300

    Acesso ao BD de facilitadores do accesseduca

---
+++ server/boot/script.js
@@ -0,0 +1,42 @@
+module.exports = function (app) {
+  var mongoDB = app.dataSources.mongoDS;
+  mongoDB.autoupdate('Usuario', function (err) {
+    if (err) throw (err);
+    var Usuario = app.models.Usuario;
+    Usuario.find({ where: { username: 'admin' }, limit: 1 }, function (err, users) {
+      if (users.length == 0) {
+        Usuario.create([
+          { username: 'admin', email: 'admin@admin.com', password: 'admin', admin: true }
+        ], function (err, users) {
+          if (err) throw(err);
+
+          var Role = app.models.Role;
+          var RoleMapping = app.models.RoleMapping;
+
+          Role.destroyAll();
+          RoleMapping.destroyAll();
+
+          //create the admin role
+          Role.create({
+            name: 'admin'
+          }, function (err, role) {
+            if (err) throw(err);
+
+            //make admin
+            role.principals.create({
+              principalType: RoleMapping.USER,
+              principalId: users[0].id
+            }, function (err, principal) {
+              if (err) throw (err);
+            });
+          });
+        })
+      }
+      else {
+      }
+    });
+  });
+
+};
+
+//{"username":"admin","password":"admin"}
